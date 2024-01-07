5f1e01103a087a027cf7321109ab3bd3e722a07c
Azimbek <89328814+azimbekxaydarov946@users.noreply.github.com>
2024-01-07 21:46:33 +0500

    Update ExampleTest.php

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -14,7 +14,7 @@ class ExampleTest extends TestCase
      */
     public function test_example()
     {
-        $response = $this->get('/');
+        $response = $this->get('/en');
 
         $response->assertStatus(200);
     }
